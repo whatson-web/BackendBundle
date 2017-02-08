fad551474bbd8ff9fcbc4e07236be5d4d9188e0d
JF <jf@whatson-web.com>
2017-02-08 14:36:19 +0100

    refactoring

--- Services/Backend/UpdateController.php
+++ Services/Backend/UpdateController.php
@@ -21,7 +21,20 @@ class UpdateController extends BaseController implements BaseControllerInterface
 
     protected $container;
 
-    private $modal = false;
+    public $modal = false;
+
+    public $renderVars;
+
+    public $config;
+    public $globalConfig;
+
+    public $entityPathConfig;
+    public $request;
+    public $data;
+
+    public $form;
+    public $formFields;
+    public $footerFormFields;
 
     /**
      * SearchController constructor.
@@ -42,6 +55,9 @@ class UpdateController extends BaseController implements BaseControllerInterface
      */
     public function update($entityPathConfig, $id, Request $request)
     {
+        $this->entityPathConfig = $entityPathConfig;
+        $this->request = $request;
+
         $em = $this->container->get('doctrine')->getManager();
 
         $data = $em->getRepository($this->getRepositoryName($entityPathConfig))->get(
@@ -57,169 +73,86 @@ class UpdateController extends BaseController implements BaseControllerInterface
             return $this->redirect($this->getActionUrl($entityPathConfig, 'index'));
         }
 
-        $renderVars = array();
+        $this->data = $data;
 
-        $config = $this->getConfig($entityPathConfig, 'update');
-        $globalConfig = $this->getGlobalConfig($entityPathConfig);
+        $this->config = $this->getConfig($entityPathConfig, 'update');
+        $this->globalConfig = $this->getGlobalConfig($entityPathConfig);
 
-        $renderVars['globalConfig'] = $globalConfig;
+        $this->renderVars['globalConfig'] = $this->globalConfig;
 
-        $renderVars['title'] = $config['title'];
+        $this->createUpdateForm();
+        if ($this->request->getMethod() == 'POST') {
+            return $this->handleUpdateFormSubmission();;
+        }
+        $this->renderUpdateForm();
 
-        $formFields = $this->getFormFields($config['formFields'], $entityPathConfig);
+        $this->getFormProperties();
 
-        $form = $this->getEntityForm($formFields, $entityPathConfig, $data);
+        $this->renderVars['title'] = $this->config['title'];
 
         if (!$this->modal) {
-            $renderVars['breadcrumb'] = $this->getBreadcrumb(
-                $config['breadcrumb'],
+            $this->renderVars['breadcrumb'] = $this->getBreadcrumb(
+                $this->config['breadcrumb'],
                 $entityPathConfig,
                 $data
             );
-        } else {
-            $footerFormFields = $config['footerFormFields'];
-
-            foreach ($footerFormFields as $footerFormField) {
-                $form->add(
-                    $footerFormField['field'],
-                    SubmitType::class,
-                    array(
-                        'label' => $footerFormField['label'],
-                    )
-                );
-            }
-        }
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted()) {
-            $data = $form->getData();
-
-            $em->persist($data);
-            $em->flush();
-
-            $redirectUrl = $this->getActionUrl($entityPathConfig, 'index', $data);
-            if ($form->has('saveAndStay') && $form->get('saveAndStay')->isClicked()) {
-                $redirectUrl = $this->getActionUrl($entityPathConfig, 'update', $data);
-            }
-
-            if ($request->isXmlHttpRequest()) {
-                return new JsonResponse(
-                    array(
-                        'success'  => true,
-                        'redirect' => $redirectUrl,
-                    )
-                );
-            }
-
-            return $this->redirect($redirectUrl);
-        } else {
-            $form->setData($data);
-        }
-
-        $form = $form->createView();
-        $renderVars['form'] = $form;
-        $renderVars['formFields'] = $formFields;
-
-        if (!$this->modal) {
-            if (!empty($config['central']['viewLink']['action'])) {
-                $config['central']['viewLink']['url'] = $this->getActionUrl(
-                    $entityPathConfig,
-                    $config['central']['viewLink']['action'],
-                    $data
-                );
-            }
-
-            foreach ($config['central']['tabs'] as $tabSlug => $tabProperties) {
-                if (isset($tabProperties['iframeContent'])) {
-                    $config['central']['tabs'][$tabSlug]['iframeContent']['url'] = $this->getActionUrl(
-                        $entityPathConfig,
-                        $tabProperties['iframeContent']['action'],
-                        $data
-                    );
-                }
-            }
-
-            $renderVars['central'] = $config['central'];
-
-            foreach ($config['column']['panelZones'] as $key => $panelZone) {
-                $panelZone['form'] = $form;
-                $panelZone['formFields'] = $this->getFormFields($panelZone['fields'], $entityPathConfig);
-
-                unset($panelZone['fields']);
-
-                if (isset($panelZone['footerListFormButtons'])) {
-                    foreach ($panelZone['footerListFormButtons'] as $field => $footerListFormButton) {
-                        $footerListFormButton = array_merge($footerListFormButton, $config['formFields'][$field]);
-                        $footerListFormButton['form'] = $form;
-
-                        $panelZone['footerListFormButtons'][$field] = $footerListFormButton;
-                    }
-                }
-                $config['column']['panelZones'][$key] = $panelZone;
-            }
-
-            $renderVars['column'] = $config['column'];
-        } else {
-            $renderVars['footerFormFields'] = $footerFormFields;
-            $renderVars['formAction'] = $this->getActionUrl($entityPathConfig, 'update', $data);
         }
 
         $view = '@WHBackendTemplate/BackendTemplate/View/update.html.twig';
         if ($this->modal) {
             $view = '@WHBackendTemplate/BackendTemplate/View/modal.html.twig';
-            if (isset($config['view'])) {
-                $view = $config['view'];
+            if (isset($this->config['view'])) {
+                $view = $this->config['view'];
             }
         }
 
-        $renderVars = $this->translateRenderVars($entityPathConfig, $renderVars);
+        $this->renderVars = $this->translateRenderVars($entityPathConfig, $this->renderVars);
 
         return $this->container->get('templating')->renderResponse(
             $view,
-            $renderVars
+            $this->renderVars
         );
     }
 
     /**
-     * @param $config
-     *
      * @return bool
      */
     public function validConfig($config)
     {
-        if (isset($config['modal']) && $config['modal'] == 'true') {
-            $this->validConfigPopup($config);
+        $this->config = $config;
+
+        if (isset($this->config['modal']) && $this->config['modal'] == 'true') {
+            $this->validConfigPopup($this->config);
             $this->modal = true;
         } else {
-            $this->validConfigClassic($config);
+            $this->validConfigClassic($this->config);
         }
 
         return true;
     }
 
     /**
-     * @param $config
-     *
-     */
-    public function validConfigPopup($config)
-    {
-        if (!isset($config['formFields'])) {
-
-            throw new NotFoundHttpException('Le fichier de configuration ne contient pas le champ "formFields"');
-        }
-    }
-
-    /**
-     * @param $config
-     *
      * @return bool
      */
-    public function validConfigClassic($config)
+    public function validConfigPopup()
     {
+        if (!isset($this->config['formFields'])) {
+            throw new NotFoundHttpException('Le fichier de configuration ne contient pas le champ "formFields"');
+        }
+
+        return true;
     }
 
     /**
+     * @return bool
+     */
+    public function validConfigClassic()
+    {
+        return true;
+    }
+
+    /**
+     * @param $entityPathConfig
      * @param $renderVars
      *
      * @return mixed
@@ -291,4 +224,132 @@ class UpdateController extends BaseController implements BaseControllerInterface
 
         return $renderVars;
     }
+
+    /**
+     * @return bool|JsonResponse|\Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function createUpdateForm()
+    {
+        $this->formFields = $this->getFormFields($this->config['formFields'], $this->entityPathConfig);
+
+        $this->form = $this->getEntityForm($this->formFields, $this->entityPathConfig, $this->data);
+
+        if ($this->modal) {
+            $this->footerFormFields = $this->config['footerFormFields'];
+
+            foreach ($this->footerFormFields as $footerFormField) {
+                $this->form->add(
+                    $footerFormField['field'],
+                    SubmitType::class,
+                    array(
+                        'label' => $footerFormField['label'],
+                    )
+                );
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * @return bool|JsonResponse|\Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function handleUpdateFormSubmission()
+    {
+        $this->form->handleRequest($this->request);
+
+        if ($this->form->isSubmitted()) {
+            $data = $this->form->getData();
+
+            $em = $this->get('doctrine')->getManager();
+
+            $em->persist($data);
+            $em->flush();
+
+            $redirectUrl = $this->getActionUrl($this->entityPathConfig, 'index', $data);
+            if ($this->form->has('saveAndStay') && $this->form->get('saveAndStay')->isClicked()) {
+                $redirectUrl = $this->getActionUrl($this->entityPathConfig, 'update', $data);
+            }
+
+            if ($this->request->isXmlHttpRequest()) {
+                return new JsonResponse(
+                    array(
+                        'success'  => true,
+                        'redirect' => $redirectUrl,
+                    )
+                );
+            }
+
+            return $this->redirect($redirectUrl);
+        } else {
+            $this->form->setData($this->data);
+        }
+
+        return true;
+    }
+
+    /**
+     * @return bool
+     */
+    public function renderUpdateForm()
+    {
+        $this->form = $this->form->createView();
+        $this->renderVars['form'] = $this->form;
+        $this->renderVars['formFields'] = $this->formFields;
+
+        return true;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getFormProperties()
+    {
+        if (!$this->modal) {
+            if (!empty($this->config['central']['viewLink']['action'])) {
+                $this->config['central']['viewLink']['url'] = $this->getActionUrl(
+                    $this->entityPathConfig,
+                    $this->config['central']['viewLink']['action'],
+                    $this->data
+                );
+            }
+
+            foreach ($this->config['central']['tabs'] as $tabSlug => $tabProperties) {
+                if (isset($tabProperties['iframeContent'])) {
+                    $this->config['central']['tabs'][$tabSlug]['iframeContent']['url'] = $this->getActionUrl(
+                        $this->entityPathConfig,
+                        $tabProperties['iframeContent']['action'],
+                        $this->data
+                    );
+                }
+            }
+
+            $this->renderVars['central'] = $this->config['central'];
+
+            foreach ($this->config['column']['panelZones'] as $key => $panelZone) {
+                $panelZone['form'] = $this->form;
+                $panelZone['formFields'] = $this->getFormFields($panelZone['fields'], $this->entityPathConfig);
+
+                unset($panelZone['fields']);
+
+                if (isset($panelZone['footerListFormButtons'])) {
+                    foreach ($panelZone['footerListFormButtons'] as $field => $footerListFormButton) {
+                        $footerListFormButton = array_merge($footerListFormButton, $this->config['formFields'][$field]);
+                        $footerListFormButton['form'] = $this->form;
+
+                        $panelZone['footerListFormButtons'][$field] = $footerListFormButton;
+                    }
+                }
+                $this->config['column']['panelZones'][$key] = $panelZone;
+            }
+
+            $this->renderVars['column'] = $this->config['column'];
+        } else {
+            $this->renderVars['footerFormFields'] = $this->footerFormFields;
+            $this->renderVars['formAction'] = $this->getActionUrl($this->entityPathConfig, 'update', $this->data);
+        }
+
+        return true;
+    }
+
 }
